6bacac853e5e39aaa30cbe8d77cb93dbcb7a4d36
Alex <alexcascante.689@yahoo.com>
2020-10-25 21:13:46 -0600

    Index_General_V2

--- BDAseguradora/BDAseguradora/JS/main.js
+++ BDAseguradora/BDAseguradora/JS/main.js
@@ -10,6 +10,32 @@ $('.toggle').click(function () {
     var x=false;
 });
 
+(function () {
+    "use strict"
+
+
+
+    var regalo = document.getElementById('regalo');
+    document.addEventListener('DOMContentLoaded', function () {
+
+        //MAP
+        var map = L.map('mapa').setView([9.846612, -84.31568], 17);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([9.846612, -84.31568]).addTo(map)
+            .bindPopup('Estás Aquí')
+            .openPopup('ads') //FIN MAPA
+            .bindTooltip('Un punto')        //descripción en el mapa
+            .openTooltip('asd')
+
+    }); //DOM CONTENT LOADED
+})();
+
+
+
 
 
 
